95f020d8c1f576bc3b39cce930812e28e4934a7a
MatthyPlayz <cjmatthy200@gmail.com>
2019-04-27 12:07:55 -0400

    1.0.1

---
+++ build/sources/main/java/chemistree/Reference.java
@@ -0,0 +1,8 @@
+package chemistree;
+
+public class Reference {
+	public static final String MODID = "chemistree";
+	public static final String MODNAME = "ChemisTREE";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
