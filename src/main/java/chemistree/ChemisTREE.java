87cbdbf7faf8953ce503d9ff0af81c739b84bacb
MatthyPlayz <cjmatthy200@gmail.com>
2019-04-28 07:53:06 -0400

    Error to update

--- src/main/java/chemistree/ChemisTREE.java
+++ src/main/java/chemistree/ChemisTREE.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION, acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS, dependencies = "required-after:saltmod")
+@Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION, acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS, dependencies = "required-after:saltmod;")
 public class ChemisTREE {
 	@Instance
 	public static ChemisTREE instance;
